f745ec9c0b822ba3b83d2c74b9b4fa02941af3d5
Rowan Copley <rowan.copley@gmail.com>
2023-04-27 20:32:47 -0700

    playing with speech recognition

--- transcription.py
+++ transcription.py
@@ -6,6 +6,10 @@ model = whisper.load_model("small.en")
 r = sr.Recognizer()
 
 def listen(limit_sec=10):
+    fname = record(limit_sec)
+    return model.transcribe("temp.wav", fp16=False)['text']
+
+def record(limit_sec=10, fname='temp.wav'):
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         subprocess.run(['afplay', '/System/Library/Sounds/Blow.aiff'])
@@ -13,7 +17,6 @@ def listen(limit_sec=10):
         audio = r.listen(source, timeout=5, phrase_time_limit=limit_sec)
         subprocess.run(['afplay', '/System/Library/Sounds/Bottle.aiff'])
         # !afplay /System/Library/Sounds/Bottle.aiff
-    with open('temp.wav', 'wb') as f:
+    with open(fname, 'wb') as f:
         f.write(audio.get_wav_data())
-    return model.transcribe("temp.wav", fp16=False)['text']
-
+    return fname
